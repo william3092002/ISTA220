131159b6a16d8908c8439914afea307d42a6c2ba
William Chan <chanh5@my.erau.edu>
2020-07-27 14:27:42 -0400

    added quizes

---
+++ quiz/quiz02/quiz02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace quiz02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is C Sharp quiz 2.");
+            Console.Write("Please enter the number of integers to add: ");
+            string strNumber = Console.ReadLine();
+            int end = Int32.Parse(strNumber);
+            int start = 0;
+            int sum = 0;
+            sum = GetSums(start, end, sum);
+            Console.WriteLine($"the sum is { sum}");
+        }
+
+        private static int GetSums(int start, int end, int sum)
+        {
+
+            if (end != 0)
+            {
+                sum = end + GetSums(start, end - 1 , sum);
+                Console.WriteLine($"The sum of {end} and {sum - end} is {sum}");
+                return sum;
+            }
+            else
+                Console.WriteLine($"The sum of {end} and {sum - end} is {sum}");
+            return sum;
+        }
+    }
+}
